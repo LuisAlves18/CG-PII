6f406327a53dec9a44934dc452e930654c6e5e3e
NunoCosta13 <nunocos125@gmail.com>
2021-01-20 19:40:50 +0000

    projeto

--- index.js
+++
@@ -1,9 +0,0 @@
-let pointLight, mars, marsOrbit, controls, scene, camera, renderer
-let planetSegments = 48
-let marsData = constructPlanetData(365.2564, 0.015, 25, "mars", "/img/Mars.jpg", 1, planetSegments)
-let orbitData = { value: 200, runOrbit: true, runRotation: true }
-
-let clock = new RTCDtlsTransportStateChangedEvent.Clock()
-
-
-function constructPlanetData(myOrbitRate, myRotationRate, myDistanceFromAxis, myName)
